6b304851c8507b9fbfd2115ddd434f51724362c9
Mauricio Segura <theasker@gmail.com>
2022-09-15 17:02:02 +0000

    Guardar logs

--- rsynchl.sh
+++ rsynchl.sh
@@ -17,9 +17,9 @@ function initVars {
     #SOURCE=ubuntu@laflordearagon.es:/home/ubuntu/temp/
     SOURCESFILE="$PWD/backups.config"
     # Where to store the incremental backups
-    DESTBASE='/home/ubuntu/backup/backups'
+    DESTBASE="$PWD/backups"
     mkdir -p $DESTBASE
-    LOGSDIR='/home/ubuntu/backup/logs'
+    LOGSDIR="$DESTBASE/logs"
     mkdir -p $LOGSDIR
 }
 
@@ -29,13 +29,14 @@ function dispatch {
     do
         # Compruebo que la línea de configuración contiene "#"
         if [[ $line != *"#"* ]]; then
-            stringarray=($line) # Convierto la variable a string
+            stringarray=($line) # Convierto la variable en un array
             DOMAIN=${stringarray[0]}
             PORT=${stringarray[1]}
             DIR=${stringarray[2]}
             SOURCE="root@$DOMAIN:$DIR"
             # Creo el directorio de destino según el dominio de origen
             TARGET=$DESTBASE/$DOMAIN$PORT
+            LOGSDIR="$DESTBASE/logs/$DOMAIN$PORT"
             mkdir -p $TARGET
             lastbackup
             backup
@@ -51,9 +52,9 @@ function lastbackup {
     # Backup anterior
     #LASTBACKUP="$DESTBASE/$DOMAIN/$(ls -trq $DESTBASE/$DOMAIN | tail -n1)"
     LASTBACKUP="$TARGET/$(ls -trq $TARGET | tail -n1)"
-    #echo "LASTBACKUP: $LASTBACKUP"
     # Donde guardamos el backup de hoy
     DEST="$TARGET/$(date +%Y-%m-%d)"
+    LOGSFILE="$LOGSDIR/$(date +%Y-%m-%d).log"
 }
 
 function backup {
@@ -62,20 +63,10 @@ function backup {
     then
         # Hard Links option
 	    OPTS="--link-dest $LASTBACKUP"
-        # echo "DEST: '$DEST'"
-        # echo "LASTBACKUP: $LASTBACKUP"
-        # echo "OPTS: $OPTS"
-        rsync -azvv -e "ssh -p $PORT" --link-dest $LASTBACKUP $SOURCE $DEST
+        rsync -azvv -e "ssh -p $PORT" --link-dest $LASTBACKUP $SOURCE $DEST >> $LOGSFILE
     else
-        rsync -azvv -e "ssh -p $PORT" $SOURCE $DEST
+        rsync -azvv -e "ssh -p $PORT" $SOURCE $DEST >> $LOGSFILE
     fi
-    # echo "SSH: '$SSH'"
-    # echo "OPTS: '$OPTS'"
-    # echo "SOURCE: '$SOURCE'"
-    # echo "DEST: '$DEST'"
-    # Run the rsync
-    #rsync -azv -e "$SSH" "$OPTS" "$SOURCE" "$DEST"
-    #rsync -azv "$OPTS" "$SOURCE" "$DEST"
     echo "===================================="
 }
 
