35bf0c23157033bc44f3c008bc0b7f530eabddce
Mauricio Segura <theasker@gmail.com>
2022-09-15 12:01:03 +0000

    control de comentario en líneas de fichero de backups a realizar

--- rsynchl.sh
+++ rsynchl.sh
@@ -4,7 +4,7 @@
 # title: ScriptBackup.sh
 # description:  Automatic Backup Script in Bash Shell
 # author: Mauricio Segura Ariño
-# date: 20220920
+# date: 20220915
 # usage:    Create external file with the source directories
 #           bash ScriptBackup.sh
 # notes: Configure the initVars function and init de variables
@@ -27,15 +27,21 @@ function dispatch {
     # Recorremos el fichero línea a línea
     while read -r line
     do
-        stringarray=($line) # Convierto la variable a string
-        DOMAIN=${stringarray[0]}
-        PORT=${stringarray[1]}
-        DIR=${stringarray[2]}
-        SOURCE="root@$DOMAIN:$DIR"
-        # Creo el directorio de destino según el dominio de origen
-        mkdir -p $DESTBASE/$DOMAIN
-        lastbackup
-        backup
+        # Compruebo que la línea de configuración contiene "#"
+        if [[ $line != *"#"* ]]; then
+            stringarray=($line) # Convierto la variable a string
+            DOMAIN=${stringarray[0]}
+            PORT=${stringarray[1]}
+            DIR=${stringarray[2]}
+            SOURCE="root@$DOMAIN:$DIR"
+            # Creo el directorio de destino según el dominio de origen
+            TARGET=$DESTBASE/$DOMAIN$PORT
+            mkdir -p $TARGET
+            lastbackup
+            backup
+        fi
+
+        
     done < $SOURCESFILE
 }
 
@@ -43,10 +49,11 @@ function lastbackup {
     # Backup de ayer
     #YESTERDAY="$DESTBASE/$DOMAIN/$(date -d yesterday +%Y-%m-%d)/"
     # Backup anterior
-    LASTBACKUP="$DESTBASE/$DOMAIN/$(ls -trq $DESTBASE/$DOMAIN | tail -n1)"
-    echo "LASTBACKUP: $LASTBACKUP"
+    #LASTBACKUP="$DESTBASE/$DOMAIN/$(ls -trq $DESTBASE/$DOMAIN | tail -n1)"
+    LASTBACKUP="$TARGET/$(ls -trq $TARGET | tail -n1)"
+    #echo "LASTBACKUP: $LASTBACKUP"
     # Donde guardamos el backup de hoy
-    DEST="$DESTBASE/$DOMAIN/$(date +%Y-%m-%d)"
+    DEST="$TARGET/$(date +%Y-%m-%d)"
 }
 
 function backup {
@@ -55,17 +62,17 @@ function backup {
     then
         # Hard Links option
 	    OPTS="--link-dest $LASTBACKUP"
-        echo "DEST: '$DEST'"
-        echo "LASTBACKUP: $LASTBACKUP"
-        echo "OPTS: $OPTS"
-        rsync -azv -e "ssh -p $PORT" --link-dest $LASTBACKUP $SOURCE $DEST
+        # echo "DEST: '$DEST'"
+        # echo "LASTBACKUP: $LASTBACKUP"
+        # echo "OPTS: $OPTS"
+        rsync -azvv -e "ssh -p $PORT" --link-dest $LASTBACKUP $SOURCE $DEST
     else
-        rsync -azv -e "ssh -p $PORT" $SOURCE $DEST
+        rsync -azvv -e "ssh -p $PORT" $SOURCE $DEST
     fi
-    echo "SSH: '$SSH'"
-    echo "OPTS: '$OPTS'"
-    echo "SOURCE: '$SOURCE'"
-    echo "DEST: '$DEST'"
+    # echo "SSH: '$SSH'"
+    # echo "OPTS: '$OPTS'"
+    # echo "SOURCE: '$SOURCE'"
+    # echo "DEST: '$DEST'"
     # Run the rsync
     #rsync -azv -e "$SSH" "$OPTS" "$SOURCE" "$DEST"
     #rsync -azv "$OPTS" "$SOURCE" "$DEST"
@@ -74,4 +81,3 @@ function backup {
 
 initVars
 dispatch
-# backup
